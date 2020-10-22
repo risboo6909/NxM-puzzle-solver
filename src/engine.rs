9695d052ea93a6609549afa1f8e72eb57a000023
risboo6909 <ttyv00@gmail.com>
2020-10-22 17:23:16 +0300

    Limit max recur depth + add 4x4 puzzle example

--- src/engine.rs
+++ src/engine.rs
@@ -9,12 +9,17 @@ const STACK_PER_RECURSION: usize = 1 * 1024 * 1024; // 1MB
 // search space reduces significantly
 const RELAXATION_COEFF: isize = 7;
 
+// evetually, maximum length of winning sequence of moves
+const MAX_DEPTH: usize = 250;
+
 pub(crate) struct Engine<const HEIGHT: usize, const WIDTH: usize> {
     start_board: Board<HEIGHT, WIDTH>,
     target_board: Board<HEIGHT, WIDTH>,
     visited: FxHashMap<Board<HEIGHT, WIDTH>, usize>,
     best: Vec<Dir>,
-    counter: u128,
+    moves_counter: usize,
+    depth: usize,
+    max_depth: usize,
 }
 
 pub fn ensure_sufficient_stack<R, F: FnOnce() -> R>(f: F) -> R {
@@ -31,12 +36,22 @@ impl<const HEIGHT: usize, const WIDTH: usize> Engine<HEIGHT, WIDTH> {
             target_board,
             visited: FxHashMap::default(),
             best: vec![],
-            counter: 0,
+            moves_counter: 0,
+            depth: 0,
+            max_depth :0,
         }
     }
 
     #[inline(never)]
     fn rec(&mut self, board: &mut Board<HEIGHT, WIDTH>, cur: &mut Vec<Dir>) {
+        if self.depth > self.max_depth {
+            self.max_depth = self.depth;
+        }
+        
+        if self.depth >= MAX_DEPTH {
+            return;
+        }
+
         if let Some(l) = self.visited.get(board) {
             if *l as isize - cur.len() as isize <= RELAXATION_COEFF {
                 return;
@@ -44,7 +59,7 @@ impl<const HEIGHT: usize, const WIDTH: usize> Engine<HEIGHT, WIDTH> {
         }
 
         self.visited.insert(*board, cur.len());
-        self.counter += 1;
+        self.moves_counter += 1;
 
         if board == &self.target_board {
             if cur.len() < self.best.len() || self.best.is_empty() {
@@ -57,7 +72,9 @@ impl<const HEIGHT: usize, const WIDTH: usize> Engine<HEIGHT, WIDTH> {
         if board.can_move_left() {
             cur.push(Dir::Left);
             board.move_left();
+            self.depth += 1;
             ensure_sufficient_stack(|| self.rec(board, cur));
+            self.depth -= 1;
             board.move_right();
             cur.pop();
         }
@@ -65,7 +82,9 @@ impl<const HEIGHT: usize, const WIDTH: usize> Engine<HEIGHT, WIDTH> {
         if board.can_move_right() {
             cur.push(Dir::Right);
             board.move_right();
+            self.depth += 1;
             ensure_sufficient_stack(|| self.rec(board, cur));
+            self.depth -= 1;
             board.move_left();
             cur.pop();
         }
@@ -73,7 +92,9 @@ impl<const HEIGHT: usize, const WIDTH: usize> Engine<HEIGHT, WIDTH> {
         if board.can_move_up() {
             cur.push(Dir::Up);
             board.move_up();
+            self.depth += 1;
             ensure_sufficient_stack(|| self.rec(board, cur));
+            self.depth -= 1;
             board.move_down();
             cur.pop();
         }
@@ -81,17 +102,19 @@ impl<const HEIGHT: usize, const WIDTH: usize> Engine<HEIGHT, WIDTH> {
         if board.can_move_down() {
             cur.push(Dir::Down);
             board.move_down();
+            self.depth += 1;
             ensure_sufficient_stack(|| self.rec(board, cur));
+            self.depth -= 1;
             board.move_up();
             cur.pop();
         }
     }
 
-    pub(crate) fn solve(&mut self) -> (Vec<Dir>, u128) {
-        self.counter = 0;
+    pub(crate) fn solve(&mut self) -> (Vec<Dir>, usize, usize) {
+        self.moves_counter = 0;
 
         self.rec(&mut self.start_board.clone(), &mut vec![]);
 
-        (self.best.clone(), self.counter)
+        (self.best.clone(), self.moves_counter, self.max_depth)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -32,4 +32,19 @@ fn main() {
     let res = eng.solve();
 
     println!("{:?}", res);
+
+    // example puzzle from https://youtu.be/0RDzVeRDGA0?t=224
+
+    println!("Solving 4x4...");
+
+    let start_pos: [[usize; 4]; 4] = [[7, 13, 2, 11], [8, 10, 9, 6], [14, 1, 4, 12], [3, 5, 15, 0]];
+    let target_pos: [[usize; 4]; 4] = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 0]];
+
+    let start_board = board::Board::<4, 4>::new(start_pos);
+    let target_board = board::Board::<4, 4>::new(target_pos);
+
+    let mut eng = engine::Engine::new(start_board, target_board);
+    let res = eng.solve();
+
+    println!("{:?}", res);
 }
